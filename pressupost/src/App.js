46dc32cf7d7c470555b8eb132aa86d386a1a0c2a
Joel <jsboquet@gmail.com>
2022-07-06 19:22:27 +0200

    Exercici 1

--- pressupost/src/App.js
+++ pressupost/src/App.js
@@ -1,5 +1,54 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [serveis, setServeis] = useState({
+    web: false,
+    seo: false,
+    ads: false
+  });
+
+  const [pressu, setPressu] = useState(0);
+
+  const handleInputChange = (event) => {
+    setServeis({
+      ...serveis,
+      [event.target.name] : event.target.checked
+    })
+    setPressu(
+      event.target.checked ? pressu + parseInt(event.target.value) : pressu - parseInt(event.target.value)
+    )
+  };
+
+  return (
+    <>
+      <p>¿Què vols fer?</p>
+      <input
+        type="checkbox"
+        name="web"
+        value="500"
+        onChange={handleInputChange}
+      />
+      <label for="web"> Una pàgina web - 500 €</label>
+      <br />
+      <input
+        type="checkbox"
+        name="seo"
+        value="300"
+        onChange={handleInputChange}
+      />
+      <label for="seo"> Una consultoria SEO - 300 €</label>
+      <br />
+      <input
+        type="checkbox"
+        name="ads"
+        value="200"
+        onChange={handleInputChange}
+      />
+      <label for="ads"> Una campanya de Google Ads - 200 €</label>
+      <br />
+      <p>Preu: {pressu} €</p>
+    </>
+  );
 }
 
 export default App;
